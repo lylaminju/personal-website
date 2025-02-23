94ccc91a949a32535854ed99666d73462cb2a2d7
Lyla <mjuudev@gmail.com>
2025-02-23 12:43:23 -0500

    refactor: render projects by function

---
+++ data/projects.js
@@ -0,0 +1,27 @@
+export const projects = [
+  {
+    title: 'Re:Mem',
+    github: 'https://github.com/daily-journey/remem',
+    website: 'https://daily-journey.github.io/remem',
+    description:
+      'A review note that helps people memorize things efficiently by providing an optimized review cycle',
+    techStack: 'Kotlin, Spring Boot, MySQL, TypeScript, React',
+  },
+  {
+    title: 'Climick',
+    github: 'https://github.com/pmjuu/climick-client',
+    website: 'https://climick.netlify.app',
+    description: 'Climbing web game without external physics libraries',
+    techStack:
+      'JavaScript, React, Pixi.js, react-router, Redux, styled-components, Node.js, Express.js, MongoDB, Jest',
+    youtube: 'https://www.youtube.com/embed/w5qo4VSKXTo?si=VyZeqBd7YqCdg_VU',
+  },
+  {
+    title: 'Reactree',
+    github: 'https://github.com/pmjuu/reactree-frontend',
+    description:
+      'Electron app that visualizes the component hierarchy of a React project',
+    techStack:
+      'JavaScript, React, Electron, d3.js, Redux, styled-components, Jest, playwright',
+  },
+];
---
+++ utils/renderProjects.js
@@ -0,0 +1,68 @@
+import { projects } from '../data/projects.js';
+
+function createProjectBox(project) {
+  const projectBox = document.createElement('div');
+  projectBox.className = 'project-box';
+  projectBox.onclick = () =>
+    window.open(project.website || project.github, '_blank');
+
+  projectBox.innerHTML = `
+    <div class="title-row">
+      <h3>${project.title}</h3>
+      <ul>
+        <li>
+          <a target="_blank" href="${project.github}"
+            onclick="event.stopPropagation()">
+            <svg class="link-icon">
+              <use href="../assets/github.svg#icon"></use>
+            </svg>
+          </a>
+        </li>
+        ${
+          project.website
+            ? `
+                <li>
+                    <a target="_blank" href="${project.website}" onclick="event.stopPropagation()">
+                    <svg class="link-icon">
+                        <use href="../assets/web.svg#icon"></use>
+                    </svg>
+                    </a>
+                </li>
+            `
+            : ''
+        }
+      </ul>
+    </div>
+    <p class="explanation">${project.description}</p>
+    ${
+      project.youtube
+        ? `
+            <iframe
+                width="560"
+                height="315"
+                src="${project.youtube}"
+                title="YouTube video player"
+                frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                referrerpolicy="strict-origin-when-cross-origin"
+                allowfullscreen
+            ></iframe>
+            <br />
+        `
+        : ''
+    }
+    <p class="tech-stack">⚙️ ${project.techStack}</p>
+  `;
+
+  return projectBox;
+}
+
+function renderProjects() {
+  const container = document.getElementById('projects-container');
+
+  projects.forEach((project) => {
+    container.appendChild(createProjectBox(project));
+  });
+}
+
+document.addEventListener('DOMContentLoaded', renderProjects);
