a7127dcecedda381ed0b69412016d03b17fdfd12
Lyla <mjuudev@gmail.com>
2025-02-25 14:17:10 -0500

    refactor: use posts object instead of json to render posts in blog.html

---
+++ data/posts.js
@@ -0,0 +1,14 @@
+export const posts = [
+  {
+    date: '2025-02-22',
+    title: 'Parsing Markdown to HTML',
+  },
+  {
+    date: '2025-02-23',
+    title: 'Why use `LIMIT 1` even if only one result is expected?',
+  },
+  {
+    date: '2025-02-25',
+    title: 'Hashing passwords using only the web crypto API',
+  },
+];
