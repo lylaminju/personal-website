d280297a498fc9620219443d5f7d76ec0b504783
Lyla <mjuudev@gmail.com>
2025-04-18 09:56:50 -0400

    chore: change linkedin url and email

--- components/footer.js
+++ components/footer.js
@@ -1,21 +1,21 @@
 class Footer extends HTMLElement {
-  // dynamically get base path due to posts html
-  getBasePath() {
-    const depth = window.location.pathname.split('/').filter(Boolean).length;
+	// dynamically get base path due to posts html
+	getBasePath() {
+		const depth = window.location.pathname.split("/").filter(Boolean).length;
 
-    return depth === 0 ? './' : '../'.repeat(depth);
-  }
+		return depth === 0 ? "./" : "../".repeat(depth);
+	}
 
-  constructor() {
-    super();
-  }
+	constructor() {
+		super();
+	}
 
-  connectedCallback() {
-    const iconPath = `${this.getBasePath()}assets/icons`;
+	connectedCallback() {
+		const iconPath = `${this.getBasePath()}assets/icons`;
 
-    this.innerHTML = `
+		this.innerHTML = `
       <footer>
-        <a target="blank" href="mailto:mjuudev@gmail.com">
+        <a target="blank" href="mailto:lylaminju@gmail.com">
           <svg class="footer-icon">
             <use href="${iconPath}/email.svg#icon"></use>
           </svg>
@@ -25,16 +25,16 @@ class Footer extends HTMLElement {
             <use href="${iconPath}/github.svg#icon"></use>
           </svg>
         </a>
-        <a target="blank" href="https://www.linkedin.com/in/lylaminjupark">
+        <a target="blank" href="https://www.linkedin.com/in/lylaminju">
           <svg class="footer-icon">
             <use href="${iconPath}/linkedin.svg#icon"></use>
           </svg>
         </a>
       </footer>
     `;
-  }
+	}
 }
 
 export default function registerFooter() {
-  customElements.define('app-footer', Footer);
+	customElements.define("app-footer", Footer);
 }
