a669e8ec9ebef76796fc3e2ae779e1a24f876c5c
Lyla <mjuudev@gmail.com>
2026-01-28 08:29:52 -0500

    refactor: remove redundant post sorting logic

--- components/blog-posts.js
+++ components/blog-posts.js
@@ -1,41 +1,36 @@
 import { posts } from "../data/posts.js";
 
 class BlogPosts extends HTMLElement {
-  constructor() {
-    super();
-  }
+	constructor() {
+		super();
+	}
 
-  connectedCallback() {
-    this.render();
-  }
+	connectedCallback() {
+		this.render();
+	}
 
-  render() {
-    // Sort posts by date in reverse order (newest first)
-    const sortedPosts = [...posts].sort(
-      (a, b) => new Date(b.date) - new Date(a.date)
-    );
-
-    this.innerHTML = `
+	render() {
+		this.innerHTML = `
       <div class="blog-posts">
         <ul class="posts-list">
-          ${sortedPosts
-            .map(
-              (post) => `
-            <li class="post-item">
-              <a href="/pages/post.html?slug=${post.slug}" class="post-link">
-                <div class="post-title">${post.title}</div>
-                <div class="post-date">${post.date}</div>
-              </a>
-            </li>
-          `
-            )
-            .join("")}
+          ${posts
+						.map(
+							(post) => `
+                <li class="post-item">
+                  <a href="/pages/post.html?slug=${post.slug}" class="post-link">
+                    <div class="post-title">${post.title}</div>
+                    <div class="post-date">${post.date}</div>
+                  </a>
+                </li>
+              `,
+						)
+						.join("")}
         </ul>
       </div>
     `;
-  }
+	}
 }
 
 export default function registerBlogPosts() {
-  customElements.define("blog-posts", BlogPosts);
+	customElements.define("blog-posts", BlogPosts);
 }
