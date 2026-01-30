36b2ea3197c7909e4c1653f8c4966617ddb8b03d
Lyla <mjuudev@gmail.com>
2026-01-30 15:03:09 -0500

    chore: css to CSS and formatting

--- data/projects.js
+++ data/projects.js
@@ -1,42 +1,44 @@
 export const projects = [
-  {
-    title: 'ClimberzDay',
-    emoji: '🧗',
-    image: '../assets/project-images/climberzday-preview.jpg',
-    github: 'https://github.com/lylaminju/climbers',
-    website: 'https://climberz.day',
-    description: 'Web service that explores climbing gyms, compares routes, and finds new climbing partners',
-    techStack: 'TypeScript, Sveltekit, Tailwindcss, Flowbite, Supabase, PostgreSQL',
-  },
-  {
-    title: 'Re:Mem',
-    emoji: '📝',
-    image: '../assets/project-images/remem-calendar.png',
-    github: 'https://github.com/daily-journey/remem',
-    website: 'https://daily-journey.github.io/remem',
-    description:
-      'Review note that helps people memorize things efficiently by providing an optimized review cycle',
-    techStack: 'Kotlin, Spring Boot, MySQL, TypeScript, React',
-  },
-  {
-    title: 'Climick',
-    emoji: '🎮',
-    image: '../assets/project-images/climick-preview.jpg',
-    github: 'https://github.com/lylaminju/climick-client',
-    website: 'https://climick.netlify.app',
-    description: 'Climbing web game without external physics libraries',
-    techStack:
-      'JavaScript, React, Pixi.js, react-router, Redux, styled-components, Node.js, Express.js, MongoDB, Jest',
-    youtube: 'https://www.youtube.com/embed/w5qo4VSKXTo?si=VyZeqBd7YqCdg_VU',
-  },
-  {
-    title: 'Reactree',
-    emoji: '🌳',
-    image: '../assets/project-images/reactree-preview.png',
-    github: 'https://github.com/lylaminju/reactree-frontend',
-    description:
-      'Electron app that visualizes the component hierarchy of a React project',
-    techStack:
-      'JavaScript, React, Electron, d3.js, Redux, styled-components, Jest, playwright',
-  },
+	{
+		title: "ClimberzDay",
+		emoji: "🧗",
+		image: "../assets/project-images/climberzday-preview.jpg",
+		github: "https://github.com/lylaminju/climbers",
+		website: "https://climberz.day",
+		description:
+			"Web service that explores climbing gyms, compares routes, and finds new climbing partners",
+		techStack:
+			"TypeScript, Sveltekit, TailwindCSS, Flowbite, Supabase, PostgreSQL",
+	},
+	{
+		title: "Re:Mem",
+		emoji: "📝",
+		image: "../assets/project-images/remem-calendar.png",
+		github: "https://github.com/daily-journey/remem",
+		website: "https://daily-journey.github.io/remem",
+		description:
+			"Review note that helps people memorize things efficiently by providing an optimized review cycle",
+		techStack: "Kotlin, Spring Boot, MySQL, TypeScript, React",
+	},
+	{
+		title: "Climick",
+		emoji: "🎮",
+		image: "../assets/project-images/climick-preview.jpg",
+		github: "https://github.com/lylaminju/climick-client",
+		website: "https://climick.netlify.app",
+		description: "Climbing web game without external physics libraries",
+		techStack:
+			"JavaScript, React, Pixi.js, react-router, Redux, styled-components, Node.js, Express.js, MongoDB, Jest",
+		youtube: "https://www.youtube.com/embed/w5qo4VSKXTo?si=VyZeqBd7YqCdg_VU",
+	},
+	{
+		title: "Reactree",
+		emoji: "🌳",
+		image: "../assets/project-images/reactree-preview.png",
+		github: "https://github.com/lylaminju/reactree-frontend",
+		description:
+			"Electron app that visualizes the component hierarchy of a React project",
+		techStack:
+			"JavaScript, React, Electron, d3.js, Redux, styled-components, Jest, playwright",
+	},
 ];
